837598ce532f0ffd70f044b9424f68256b072bfa
rohit marmat <rohitmarmat4922@gmail.com>
2025-11-27 20:39:48 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Exact frontend origin
+  origin: "*", // Exact frontend origin
   credentials: true               // Allow cookies/auth headers
 }));
 
